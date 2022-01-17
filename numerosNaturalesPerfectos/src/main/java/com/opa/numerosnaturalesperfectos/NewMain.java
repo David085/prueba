9c947c432ec0e22fb1e95be959015c684824a1b0
David085 <davidgar048@gmail.com>
2022-01-17 15:27:48 -0500

    15:27

--- numerosNaturalesPerfectos/src/main/java/com/opa/numerosnaturalesperfectos/NewMain.java
+++ numerosNaturalesPerfectos/src/main/java/com/opa/numerosnaturalesperfectos/NewMain.java
@@ -12,13 +12,13 @@ public class NewMain {
             min = sc.nextInt();
             System.out.println("Escriba el valor mayor del intervalo: ");
             max = sc.nextInt();
-            for (int i = min; i <= max; i++) {
-                for (int j = 1; j < i; j++) {
-                    if (i % j == 0) {
-                        contador += j;
+            for (int i = min; i <= max; i++) { //Con este for se recorre el rango dado
+                for (int divisor = 1; divisor < i; divisor++) { //Con este for sabemos si el numero actual es perfecto
+                    if (i % divisor == 0) {
+                        contador += divisor; // se van acumulando los divisores en la variable contador
                     }
                 }
-                if (contador == i) {
+                if (contador == i) { // contador = suma de los divisores. i = el numero que estamos evaluando
                     System.out.println(i);
                 }
                 contador = 0;
@@ -30,6 +30,14 @@ public class NewMain {
     } // fin esPerfecto
 
     public static void main(String[] args) {
+        System.out.println("------------------------------------------");
+        System.out.println("- Programa para listar números perfectos -");
+        System.out.println("------------------------------------------\n");
+        System.out.println("""
+                           Debe ingresar dos valores que corresponden
+                           al intervalo en el cual desea buscar núfameros perfectos.
+                           Tenga en cuenta que deben ser núfameros naturales
+                           """);
         esPerfecto();
     } // fin main
 
