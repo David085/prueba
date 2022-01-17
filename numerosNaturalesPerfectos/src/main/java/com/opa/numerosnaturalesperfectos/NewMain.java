41b26da891f36c76bba2158034d42854bc1fe231
David085 <davidgar048@gmail.com>
2022-01-17 12:12:57 -0500

    12:12

--- numerosNaturalesPerfectos/src/main/java/com/opa/numerosnaturalesperfectos/NewMain.java
+++ numerosNaturalesPerfectos/src/main/java/com/opa/numerosnaturalesperfectos/NewMain.java
@@ -4,25 +4,33 @@ import java.util.Scanner;
 
 public class NewMain {
 
-    public static void main(String[] args) {
-        int min, max, contador = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("ingrese un numero");
-        min = sc.nextInt();
-        System.out.println("ingrese un numero");
-        max = sc.nextInt();
-        for (int i = min; i <= max; i++) {
-            for (int j = 1; j < i; j++) {
-                if (i % j == 0) {
-                    contador += j;
+    public static void esPerfecto() {
+        try {
+            int min, max, contador = 0; //min y max son los valores extremos del rango de números
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Escriba el valor menor del intervalo: ");
+            min = sc.nextInt();
+            System.out.println("Escriba el valor mayor del intervalo: ");
+            max = sc.nextInt();
+            for (int i = min; i <= max; i++) {
+                for (int j = 1; j < i; j++) {
+                    if (i % j == 0) {
+                        contador += j;
+                    }
                 }
-            }
-            if (contador==i) {
-                System.out.println(i);
-            }
-            contador = 0;
-        }
+                if (contador == i) {
+                    System.out.println(i);
+                }
+                contador = 0;
+            } // fin for
+        } catch (Exception e) {
+            System.out.println("Error");
+        } // fin try-catch
 
+    } // fin esPerfecto
+
+    public static void main(String[] args) {
+        esPerfecto();
     } // fin main
 
 } // fin clase
