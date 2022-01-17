3854dd54dd548b5ea7c669dc7c7c811d27c72d1f
David085 <davidgar048@gmail.com>
2022-01-16 22:48:51 -0500

    22:48

--- numerosNaturalesPerfectos/src/main/java/com/opa/numerosnaturalesperfectos/NewMain.java
+++ numerosNaturalesPerfectos/src/main/java/com/opa/numerosnaturalesperfectos/NewMain.java
@@ -4,15 +4,25 @@ import java.util.Scanner;
 
 public class NewMain {
 
-    
     public static void main(String[] args) {
-        int n;
+        int min, max, contador = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("ingrese un numero");
-        n = sc.nextInt();
-        for (int i = 0; i < 10; i++) {
-            
-        } // fin for
+        min = sc.nextInt();
+        System.out.println("ingrese un numero");
+        max = sc.nextInt();
+        for (int i = min; i <= max; i++) {
+            for (int j = 1; j < i; j++) {
+                if (i % j == 0) {
+                    contador += j;
+                }
+            }
+            if (contador==i) {
+                System.out.println(i);
+            }
+            contador = 0;
+        }
+
     } // fin main
-    
+
 } // fin clase
